5e2993360b8260fa1ce41a65af9c918613561668
mdhujaifatowhid <mdhujaifa411@gmail.com>
2026-03-25 23:15:13 +0600

    Delete lib directory

--- lib/rateLimit.ts
+++
@@ -1,8 +0,0 @@
-
-const map=new Map();
-export function checkLimit(ip){
-  const now=Date.now();
-  const last=map.get(ip)||0;
-  if(now-last<3000) throw new Error("Slow down");
-  map.set(ip,now);
-}
